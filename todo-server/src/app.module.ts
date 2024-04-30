962688c9e97edec3c95c9ffb75c47bc8d9ff04a4
Sarun Maharjan <sarunmrzn@gmail.com>
2024-04-30 23:09:20 +0545

    config module

--- todo-server/src/app.module.ts
+++ todo-server/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TodosModule } from './todos/todos.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://sarun:pass@localhost:27017/'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
     TodosModule,
   ],
 })
